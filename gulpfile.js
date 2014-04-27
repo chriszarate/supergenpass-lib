778d0c1e82a8a7345f6d339f0b80962f0e8fc078
Denis Sokolov <denis@sokolov.cc>
2014-04-27 21:06:09 +0900

    gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var streamify = require('gulp-streamify');
 var uglify = require('gulp-uglify');
 
+
+gulp.task('clean', function(){
+	var clean = require('gulp-clean');
+
+	gulp.src('dist', {read: false})
+		.pipe(clean());
+});
+
 gulp.task('browserify', function(){
 	var source = require('vinyl-source-stream');
 	var browserify = require('browserify');
